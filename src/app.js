1e7241316a85281050e0ac7a65101cfc88e70efb
Guilherme <guilherme@pop-os.localdomain>
2021-02-13 11:47:42 -0300

    added mongodb database connection

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
-const express = require('express');
+const express = require('express')
+const Database = require('./bin/database')
+const db = new Database()
 
-const app = express();
+const app = express()
+db.connect()
 
-module.exports = app;
+
+module.exports = app
